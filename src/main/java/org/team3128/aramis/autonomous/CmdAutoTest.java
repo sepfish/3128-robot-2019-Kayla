2cb15d15e81bd622a7aa54a9adfcc40e6805820f
sepfish <40220408+sepfish@users.noreply.github.com>
2019-11-21 16:11:59 -0800

    ???

---
+++ src/main/java/org/team3128/aramis/autonomous/CmdAutoTest.java
@@ -0,0 +1,15 @@
+package org.team3128.button.autonomous;
+
+import org.team3128.common.util.enums.Direction;
+import org.team3128.common.util.units.Length;
+
+import org.team3128.common.drive.SRXTalonDrive;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+
+public class CMDAutoTest extends CommandGroup {
+publix CMDAutoTest () {
+
+}
+}
