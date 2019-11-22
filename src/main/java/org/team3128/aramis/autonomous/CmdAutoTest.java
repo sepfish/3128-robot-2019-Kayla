192c9386eb26ea1192f594b6efb03762bdce584e
sepfish <40220408+sepfish@users.noreply.github.com>
2019-11-21 16:14:56 -0800

    Saved

--- src/main/java/org/team3128/aramis/autonomous/CmdAutoTest.java
+++ src/main/java/org/team3128/aramis/autonomous/CmdAutoTest.java
@@ -9,7 +9,11 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 
 public class CMDAutoTest extends CommandGroup {
-publix CMDAutoTest () {
-
-}
+  public CMDAutoTest () {
+    SRXTankDrive drive = SRXTankDrive.getInstance();
+    
+    //adds commands in a sequence. The large number at the end is the number of milliseconds it will wait to do the next command if it can't do that one
+    addSequential(drive.new CmdDriveStraight(100 * Length.in, 0.5, 10000));
+    addSequential(drive.new CmdInPlaceTurn(90, Direction.LEFT, 0.5, 10000));
+  }
 }
