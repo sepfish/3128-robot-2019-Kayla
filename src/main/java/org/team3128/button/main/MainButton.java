e566fee623bc00b6519677e83129b11dc3776523
sepfish <40220408+sepfish@users.noreply.github.com>
2019-12-12 16:25:21 -0800

    I was right >:3 fix numbers later

--- src/main/java/org/team3128/button/main/MainButton.java
+++ src/main/java/org/team3128/button/main/MainButton.java
@@ -103,9 +103,9 @@ public class MainButton extends NarwhalRobot {
 		//coding vectors - x is x-component in meters (use * Constants.inchesToMeters) and so on
 		//waypoints.add(new Pose2D(double x, double y, Rotation2D.fromDegrees(angle)));
 		waypoints.add(new Pose2D(0, 0, Rotation2D.fromDegrees(0)));
-		waypoints.add(new Pose2D(0 * Constants.inchesToMeters(), 39 * Constants.inchesToMeters(), Rotation2D.fromDegrees(90)));
+		waypoints.add(new Pose2D(0 * Constants.inchesToMeters(), 39 * Constants.inchesToMeters(), Rotation2D.fromDegrees(-90)));
 		waypoints.add(new Pose2D(27 * Constants.inchesToMeters(), 39 * Constants.inchesToMeters(), Rotation2D.fromDegrees(0)));
-		waypoints.add(new Pose2D(53 * Constants.inchesToMeters(), 39 * Constants.inchesToMeters(), Rotation2D.fromDegrees(90)));
+		waypoints.add(new Pose2D(53 * Constants.inchesToMeters(), 39 * Constants.inchesToMeters(), Rotation2D.fromDegrees(-90)));
 		waypoints.add(new Pose2D(53 * Constants.inchesToMeters(), 0 * Constants.inchesToMeters(), Rotation2D.fromDegrees(0)));
 		waypoints.add(new Pose2D(53 * Constants.inchesToMeters(), -39 * Constants.inchesToMeters(), Rotation2D.fromDegrees(90)));
 		waypoints.add(new Pose2D(96 * Constants.inchesToMeters(), -39 * Constants.inchesToMeters(), Rotation2D.fromDegrees(0)));
